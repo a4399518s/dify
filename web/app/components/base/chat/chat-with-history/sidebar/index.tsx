3ef876d9ddd8e23ef4213e8f779cbd110e353957
fzh <1209103220@qq.com>
2025-04-23 10:21:44 +0800

    fix bug

--- web/app/components/base/chat/chat-with-history/sidebar/index.tsx
+++ web/app/components/base/chat/chat-with-history/sidebar/index.tsx
@@ -131,7 +131,7 @@ const Sidebar = ({ isPanel,callback }: Props) => {
         {/* <Button variant='secondary-accent' disabled={isResponding} className='w-full justify-center' onClick={handleNewConversation}> */}
         <Button variant='secondary-accent' disabled={isResponding} className='w-full justify-center' onClick={newConversation}>
           <RiEditBoxLine className='mr-1 h-4 w-4' />
-          {t('share.chat.newChat')}1111
+          {t('share.chat.newChat')}
         </Button>
       </div>
       <div className='h-0 grow space-y-2 overflow-y-auto px-3 pt-4'>
